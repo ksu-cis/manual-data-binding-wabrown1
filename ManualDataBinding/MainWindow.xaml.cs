1b52440522c7b34a5ba3752843bf8a167451b109
William Brown <wbrown2492@gmail.com>
2020-03-24 18:58:39 -0500

    Manual Data Binding with Note

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,45 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Note to add to our editor
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Event handler to create a new note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event args</param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Event handler to clear the contents of a note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event args</param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+
+        /// <summary>
+        /// Event handler to mutate a note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event args</param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Master Splinter";
+            note.Body = "There is not monster more dangerous than a lack of compassion.";
         }
     }
 }
--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -19,9 +19,46 @@ namespace ManualDataBinding.UI
     /// </summary>
     public partial class NoteEditor : UserControl
     {
+        private Note note;
+        /// <summary>
+        /// The Note that will be edited by this control
+        /// </summary>
+        public Note Note
+        {
+            get { return note; }
+            set
+            {
+                if (note != null) note.NoteChanged -= OnNoteChange;
+                note = value;
+                if (note != null)
+                {
+                    note.NoteChanged += OnNoteChange;
+                    OnNoteChange(note, new EventArgs());
+                }
+            }
+        }
+
+
         public NoteEditor()
         {
             InitializeComponent();
+        }
+
+        public void OnNoteChange(object sender, EventArgs e)
+        {
+            if (Note == null) return;
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;
+        }
+
+        public void OnTitleChange(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+        }
+
+        public void OnBodyChange(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
